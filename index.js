d95f2f5418b1c1114bc7ab15c5d21c84dc34069c
Jay min Lee <jayminlee@houkibosi-MacbookPro.local>
2019-02-19 23:15:06 +0900

    add route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var router = require("./router/main")(app);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var server = app.listen(3000, function() {
+  console.log("Express server has started on port 3000");
+  app.use(express.static("public"));
 });
